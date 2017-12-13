c3a6aae91deb00dad42bde1132f907f76ff942b6
sudhakarlahane23 <sudhakar.lahane23@gmail.com>
2017-12-13 22:18:39 +0530

    Todo list

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("form").on("submit",function(){
+        var item = $("form input");
+        var todo = {item:item.val()};
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success: function(data){
+                // Do something with the data via front-end
+                location.reload();                
+            }
+        });
+
+        return false;
+    });
+
+    $('li').on("click", function(){
+        var item = $(this).text().replace(/ /g,'-');
+        
+        $.ajax({
+            type:'DELETE',
+            url:'/todo/' + item,
+            success: function(data) {
+                location.reload();
+            }
+        });
+    })
+
+    // $("li").on("click", function(){
+    //     var item = $(this).text().replace(/ /g, "-");
+        
+    //     $.ajax({
+    //         type:'DELETE',
+    //         url:'/todo/' + item,
+    //         success: function(data) {
+    //             // Do something with the data via front-end
+    //             location.reload();
+    //         }
+
+    //     });
+    // });
+});
